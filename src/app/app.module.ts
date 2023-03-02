7b8144a14b9428ee5d360b61e8b81e4914b8ed74
p.wojkiewicz <p.wojkiewicz@gmail.com>
2023-03-02 20:48:12 +0100

    tranlsations and bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,7 +30,7 @@ import { TranslateLoader, TranslateModule } from "@ngx-translate/core";
 import { HttpClient, HttpClientModule } from "@angular/common/http";
 import { TranslateHttpLoader } from "@ngx-translate/http-loader";
 import { NgSelectModule, NgOption } from "@ng-select/ng-select";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { NgxSelectModule } from "ngx-select-ex";
 
 // AoT requires an exported function for factories
@@ -80,6 +80,7 @@ export function HttpLoaderFactory(http: HttpClient) {
     NgxSelectModule,
     FormsModule,
     NgSelectModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/pages/contact-page/contact-page.component.ts
+++ src/app/components/pages/contact-page/contact-page.component.ts
@@ -1,86 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-    selector: 'app-contact-page',
-    templateUrl: './contact-page.component.html',
-    styleUrls: ['./contact-page.component.scss']
+  selector: "app-contact-page",
+  templateUrl: "./contact-page.component.html",
+  styleUrls: ["./contact-page.component.scss"],
 })
 export class ContactPageComponent implements OnInit {
+  get name() {
+    return this.contactForm.get("name");
+  }
+  get email() {
+    return this.contactForm.get("email");
+  }
+  get message() {
+    return this.contactForm.get("message");
+  }
+  public contactForm: FormGroup;
 
-    constructor() { }
+  constructor() {
+    this.contactForm = new FormGroup({
+      name: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      message: new FormControl(null, Validators.required),
+    });
+  }
 
-    ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    if (this.contactForm.valid) {
+      const data = {
+        name: this.name,
+        email: this.email,
+        message: this.message,
+      };
+    } else {
+      this.contactForm.markAllAsTouched();
     }
-
-    pageTitleArea: pageTitle[] = [
-        {
-            title: 'Contact Us'
-        }
-    ]
-    contactInfoBox1: InfoBox1[] = [
-        {
-            icon: 'bx bx-map',
-            title: 'Our Address',
-            location: '175 5th Ave, New York, NY 10010, United States'
-        }
-    ]
-    contactInfoBox2: InfoBox2[] = [
-        {
-            icon: 'bx bx-phone-call',
-            title: 'Contact',
-            number: '(+44) - 45789 - 5789',
-            email: 'hello@wilo.com'
-        }
-    ]
-    contactInfoBox3: InfoBox3[] = [
-        {
-            icon: 'bx bx-time-five',
-            title: 'Hours of Operation',
-            text1: 'Monday - Friday: 09:00 - 20:00',
-            text2: 'Sunday & Saturday: 10:30 - 22:00'
-        }
-    ]
-
-    sectionTitle: sectionTitleContent[] = [
-        {
-            subTitle: "Get In Touch",
-            title: 'Ready to Get Started?',
-            paragraphText: 'Your email address will not be published. Required fields are marked *'
-        }
-    ]
-    contactImage: Image[] = [
-        {
-            img: 'assets/img/contact.png'
-        }
-    ]
-
-}
-class pageTitle {
-    title : string;
-}
-class InfoBox1 {
-    icon : string;
-    title : string;
-    location : string;
-}
-class InfoBox2 {
-    icon : string;
-    title : string;
-    number : string;
-    email : string;
-}
-class InfoBox3 {
-    icon : string;
-    title : string;
-    text1 : string;
-    text2 : string;
-}
-
-class sectionTitleContent {
-    subTitle : string;
-    title : string;
-    paragraphText : string;
-}
-class Image {
-    img : string;
+  }
 }
